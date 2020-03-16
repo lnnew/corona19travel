75c52c1c943470a79d2d80f303d7418a5ce659b6
hyun-new <62213378+hyun-new@users.noreply.github.com>
2020-03-16 21:43:15 +0900

    Update svgMap.js

--- dist/svgMap.js
+++ dist/svgMap.js
@@ -2896,11 +2896,11 @@ svgMap.prototype.createMap = function () {
     });*/
 
     // Tooltip events
-/*  countryElement.addEventListener('click', function (e) {
+  countryElement.addEventListener('click', function (e) {
       var countryID = countryElement.getAttribute('data-id');
       this.setTooltipContent(this.getTooltipContent(countryID));
       this.showTooltip(e);
-    }.bind(this));*/
+    }.bind(this));
     countryElement.addEventListener('hover', function (e) {
       var countryID = countryElement.getAttribute('data-id');
       this.setTooltipContent(this.getTooltipContent(countryID));
